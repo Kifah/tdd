51ea1aa350e98d7873bb5ad18ad473c1d32acd02
Kifah Abbad <k.abbad@qipu.de>
2017-07-06 12:05:22 +0200

    version without api

---
+++ api/src/AppBundle/Services/BonusCalculator.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace AppBundle\Services;
+
+
+/**
+ * Class BonusCalculator
+ * A class that calculates the bonus for a company worker, based on how long she is working in the company (in months)
+ * @package AppBundle\Services
+ */
+class BonusCalculator
+{
+    const BONUS_MULTIPLIER = 2;
+    const MAX_MONTHS = 50;
+    public function run(int $monthsAtWork): float
+    {
+        if ($this->maximumMonthsReached($monthsAtWork)) {
+            return 100;
+        }
+        return self::BONUS_MULTIPLIER * $monthsAtWork;
+    }
+    /**
+     * @param int $monthsAtWork
+     * @return bool
+     */
+    private function maximumMonthsReached(int $monthsAtWork): bool
+    {
+        return $monthsAtWork >= self::MAX_MONTHS;
+    }
+}
---
+++ api/tests/AppBundle/Services/BonusCalculatorTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Kifah Abbad
+ * Time: 11:04
+ */
+
+namespace AppBundle\Services;
+
+use PHPUnit\Framework\TestCase;
+
+
+class BonusCalculatorTest extends TestCase
+{
+
+
+    /**
+     * @test
+     */
+    public function TwoMonthsReturns4()
+    {
+        $monthsAtWork = 2;
+        $bonusCalculator = new BonusCalculator();
+        $calculatedBonus = $bonusCalculator->run($monthsAtWork);
+        $expectedBonus = 4;
+        $this->assertEquals($expectedBonus, $calculatedBonus);
+    }
+
+    /**
+     * @test
+     */
+    public function SixtyMonthsReturns100()
+    {
+        $monthsAtWork = 60;
+        $bonusCalculator = new BonusCalculator();
+        $calculatedBonus = $bonusCalculator->run($monthsAtWork);
+        $expectedBonus = 100;
+        $this->assertEquals($expectedBonus, $calculatedBonus);
+    }
+
+}
